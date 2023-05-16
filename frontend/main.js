f90f5d44fc00440c781f4133e8fcaa17728f4ebc
AidenOliver1 <94108662+AidenOliver1@users.noreply.github.com>
2023-05-16 12:48:18 +0800

    Configured the backend

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResourceCounter';
+const functionApiUrl = "https://func-azureresume-prod.azurewebsites.net/api/GetResourceCounter?code=3oe2Qofxpp_r_s70p-KwV6yTths7oaoi1lF9bfSTuKBoAzFuaIA_CA==";
+const localFunctionApi = 'http://localhost:7071/api/GetResourceCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json();
     }).then(response => {
         console.log("Website called function API.");
