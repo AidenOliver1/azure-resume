6926845d5e10488684332c6ad548d77e9a2bfb7a
AidenOliver1 <94108662+AidenOliver1@users.noreply.github.com>
2023-05-22 08:15:51 +0800

    Fixed config for function

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -747,7 +747,7 @@ const localFunctionApi = 'http://localhost:7071/api/GetResourceCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(localFunctionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json();
     }).then(response => {
         console.log("Website called function API.");
